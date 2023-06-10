22c3a6055b07d939d8164ebd4c945873bd07c870
Nelson M <nelsondmondragon@gmail.com>
2023-06-10 11:54:38 -0500

    primera version api-usuarios

---
+++ src/test/java/com/acelerati/gestionusuarios/ApiUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acelerati.gestionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
