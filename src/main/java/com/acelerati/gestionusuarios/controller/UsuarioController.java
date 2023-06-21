6c7fcec0b7e66ccd10d05f941a7b543b01cb93a3
vimabo <vimabo2908@gmail.com>
2023-06-21 08:07:05 -0500

    Feat: Modificacion de archivos por ajustes del requisito. 21 Junio 2023.

--- src/main/java/com/acelerati/gestionusuarios/controller/UsuarioController.java
+++ src/main/java/com/acelerati/gestionusuarios/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.acelerati.gestionusuarios.controller;
 import com.acelerati.gestionusuarios.dto.UsuarioDto;
 import com.acelerati.gestionusuarios.service.UsuarioService;
 import com.acelerati.gestionusuarios.service.UsuarioTipoUsuarioService;
+import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author bocanegravm
  */
 @RestController
-@RequestMapping("/api/v1.0/usuarios")
+@RequestMapping("/api/v1/usuarios")
+@Api(tags = "Gestion de usuarios",description = "Permite, Crear un usuario, Obtener un usuario por el Id," +
+        "Listar todos los usuarios del sistema  y validar un usuario.")
 public class UsuarioController {
 
     @Autowired
@@ -35,6 +39,7 @@ public class UsuarioController {
      * @author Victor Bocanegra
      */
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
+    @Operation(summary = "Listar usuarios", description ="Permite obtener todos los usuarios del sistema.")
     public ResponseEntity<List<UsuarioDto>> findAll() {
         try {
             List<UsuarioDto> list = usuarioService.getUsuarios();
@@ -52,6 +57,7 @@ public class UsuarioController {
      * @author Victor Bocanegra
      */
     @RequestMapping(value = "/{usuarioId}", method = RequestMethod.GET, produces = "application/json")
+    @Operation(summary = "Obtener usuario", description ="Permite obtener un usuario por su id.")
     public ResponseEntity<UsuarioDto> getEmpleado(@ApiParam(value = "usuarioId", required = true)
             @PathVariable int usuarioId) {
         try {
@@ -71,6 +77,7 @@ public class UsuarioController {
      * @author Victor Bocanegra
      */
     @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
+    @Operation(summary = "Crear usuario", description ="Permite Crear un usuario en el sistema.")
     public ResponseEntity<?> create(@RequestParam(value = "usuarioId", required = false, defaultValue = "0") int usuarioId,
             @RequestBody UsuarioDto request) {
         try {
@@ -86,6 +93,7 @@ public class UsuarioController {
     @RequestMapping(value = "/validar/{idUsuario}/{idTipoUsuario}", method = RequestMethod.GET,
             produces
             = "application/json")
+    @Operation(summary = "Validar usuario", description ="Permite validar por tipo de usuario.")
     public ResponseEntity<?> validar(@PathVariable Long idUsuario,
             @PathVariable Long idTipoUsuario) {
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,19 @@
 insert into tipo_usuario(descripcion,nombre)
 values
-('descripcion decano','Decano'),
-('descripcion decano programa','Director'),
-('descripcion profesor','Profesor'),
-('descripcion alumno','Alumno')
-;
+('Administrador del Sistema','Decano'),
+('Director de Programas Academicos','Director'),
+('Profesores de los Cursos','Profesor'),
+('Alumnos que integran los cursos','Alumno');
 
 insert into usuario
 (nombre, apellido,email,codigo,tipo_usuario_id)
 values
-('Usuario 1', 'apellido 1', 'usuario@gmail.com', '2333',1),
-('Usuario 2', 'apellido 2', 'usuario1@gmail.com', '2334',2),
-('Usuario 3', 'apellido 3', 'usuario2@gmail.com', '2335',3),
-('Usuario 4', 'apellido 4', 'usuario3@gmail.com', '2336',4)
-;
+('Jose Valerio', 'Perez', 'vimabo2908@gmail.com', '12345678',1),
+('Alberto Jesus', 'Martinez', 'mar3214@gmail.com', '87654321',2),
+('Ricardo', 'Rangel', 'richard12@gmail.com', '876543214',2),
+('Marta', 'Medina', 'mar43@gmail.com', '0983219483',3),
+('Lorena Isabel', 'Gonzalez', 'lore23@gmail.com', '09839483',3),
+('Julio Cesar', 'Ramirez', 'jucebo@gmail.com', '0983329483',3),
+('Carlos Andres', 'Bustamante', 'busta2134@gmail.com', '0987625342',4),
+('David Alejandro', 'Rozo', 'rozo09@gmail.com', '0123625342',4),
+('Mahisa Fernanda', 'Berrio', 'berrio999@gmail.com', '0988888342',4);
